bfd66c5bb0e645edec041bd9626930d496aecfb5
Gustavo Prizon <gustavoprizon@gmail.com>
2019-10-27 16:06:36 -0300

    criado a migrate dos students

---
+++ src/database/migrations/20191027185834-create-students.js
@@ -0,0 +1,41 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('students', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      altura: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      peso: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('students');
+  },
+};

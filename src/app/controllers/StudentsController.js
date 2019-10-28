850f660b6307821b1c7445e6f2e2b644ad00f2ed
Gustavo Prizon <gustavoprizon@gmail.com>
2019-10-28 20:45:28 -0300

    implementado o update do students

--- src/app/controllers/StudentsController.js
+++ src/app/controllers/StudentsController.js
@@ -27,9 +27,62 @@ class StudentsController {
       return res.status(400).json({ error: 'Student already exists.' });
     }
 
-    const student = await Students.create(req.body);
+    const { id, name, email, idade, altura, peso } = await Students.create(
+      req.body
+    );
 
-    return res.json(student);
+    return res.json({
+      id,
+      name,
+      email,
+      idade,
+      altura,
+      peso,
+    });
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+
+    const schema = Yup.object().shape({
+      id: Yup.number()
+        .integer()
+        .required(),
+      name: Yup.string(),
+      email: Yup.string().email(),
+      idade: Yup.number().integer(),
+      altura: Yup.number(),
+      peso: Yup.number(),
+    });
+
+    if (!(await schema.isValid({ ...req.body, id }))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { email } = req.body;
+
+    const student = await Students.findByPk(id);
+
+    if (email === student.email) {
+      const studentsExists = await Students.findOne({
+        where: { email },
+      });
+
+      if (studentsExists) {
+        return res.status(400).json({ error: 'Student already exists.' });
+      }
+    }
+
+    const { name, idade, altura, peso } = await student.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      idade,
+      altura,
+      peso,
+    });
   }
 }
 

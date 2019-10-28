cef2bd3cd4bac4587695cd953cb8fa34659a111f
Gustavo Prizon <gustavoprizon@gmail.com>
2019-10-28 19:09:35 -0300

    criado a validação de dados do  student

--- src/app/controllers/StudentsController.js
+++ src/app/controllers/StudentsController.js
@@ -1,7 +1,22 @@
+import * as Yup from 'yup';
 import Students from '../models/Students';
 
 class StudentsController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      idade: Yup.number().required(),
+      altura: Yup.number().required(),
+      peso: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const studentsExists = await Students.findOne({
       where: { email: req.body.email },
     });

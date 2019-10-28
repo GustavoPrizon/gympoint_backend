e7a9f899371be88fd883cafa14e9403a8756c0ee
Gustavo Prizon <gustavoprizon@gmail.com>
2019-10-28 17:33:37 -0300

    criada a validação para no cadastrar um student com email cadastrado

--- src/app/controllers/StudentsController.js
+++ src/app/controllers/StudentsController.js
@@ -2,6 +2,14 @@ import Students from '../models/Students';
 
 class StudentsController {
   async store(req, res) {
+    const studentsExists = await Students.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (studentsExists) {
+      return res.status(400).json({ error: 'Student already exists.' });
+    }
+
     const student = await Students.create(req.body);
 
     return res.json(student);
